26c48d3fbf6be704d79a682302a36371810cca5c
“BANTENGSELAM” <pedjuangjava@gmail.com>
2026-02-24 23:48:28 +0700

    hmmm

---
+++ penjumlahan.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() { 
+
+ int p, l, luas;
+
+ cout << "masukan panjang :";
+ cin >> p;
+ cout << "masukan luas :";
+ cin >> l;
+
+ luas = p*l;
+ cout << "hasil=" << luas;
+
+
+}
