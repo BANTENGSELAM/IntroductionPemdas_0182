bfa04904ae2ae7b021796467be9aae3227113211
“BANTENGSELAM” <pedjuangjava@gmail.com>
2026-02-24 23:36:40 +0700

    5Com

--- tes.cpp
+++ tes.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
 
+string nama;
+
+
 int main() { //muali
+    int age;
+    int age1;
+    age = 20;
+    age1 = 18;
+
     cout << "hello" << endl;
+    cout << "99" << endl;
+    cout << "age" << endl;
+    cout << "age1" << endl;
 }
