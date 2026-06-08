9bda027b7631e5b6eb02a58714baf45963f3ff42
“BANTENGSELAM” <pedjuangjava@gmail.com>
2026-06-08 16:30:06 +0700

    hitung luas

---
+++ hitungluas.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int p, l, luas;
+
+    cout << "Masukan Panjang : ";
+    cin >> p;
+
+    cout << "masukan lebar: ";
+    cin >> l;
+
+    luas = p*l;
+
+    cout << "Hasilnya = " << luas;
+}
